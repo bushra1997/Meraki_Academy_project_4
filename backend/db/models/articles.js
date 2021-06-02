0d1f4061b77d351bc2b89e5e80dedaf6065f4626
deyaa1997 <deyah.mosa@gmail.com>
2021-06-02 19:22:42 +0300

    Start File

--- backend/db/models/articles.js
+++ backend/db/models/articles.js
@@ -6,6 +6,7 @@ const articleSchema = new mongoose.Schema({
     title : {type: String , required : true},
     description : {type: String , required : true},
     img: {type: String , required :true }, 
+    comments: { type: mongoose.Schema.Types.ObjectId, ref: 'Comments' },
 })
 
 module.exports = mongoose.model("Article", articleSchema);
