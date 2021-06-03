02afad94ad5ac0a7d5a6634c18f6a1fb5d577118
Noof <noof.mohammed998@gmail.com>
2021-06-03 16:29:53 +0300

    deleteArticleById

--- backend/routers/routes/articles.js
+++ backend/routers/routes/articles.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
  
- const {createNewArticles,getAllArticles,getArticlesById,updateArticlesById} = require("../controllers/articles");
+ const {createNewArticles,getAllArticles,getArticlesById,updateArticlesById,deleteArticleById} = require("../controllers/articles");
 const articlesRouter = express.Router();
 
 articlesRouter.post("/", createNewArticles);
 articlesRouter.get("/", getAllArticles);
 articlesRouter.get("/:id", getArticlesById);
 articlesRouter.put("/:id", updateArticlesById);
+articlesRouter.delete("/:id", deleteArticleById);
 
 
 module.exports = articlesRouter;
