f476e240f8e2ee2006daa4105704186cd7ba955b
Bushra <77388684+bushra1997@users.noreply.github.com>
2021-06-03 11:19:10 +0300

    form validation done

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,52 +1,47 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-    nationality : {type: String , required : true},
-    id_number : {type: Number , required : true},
-    name: {type: String , required :true }, 
-    date_of_birth : {type :Date , required:true },
-    location: {type : String , required : true},
-    email: {type : String , required : true , unique: true},
-    password: {type : String , required : true}
-})
-
-
-users.pre('save', async function () {
-	this.email = this.email.toLowerCase();
-	this.password = await bcrypt.hash(this.password, 10);
+  nationality: { type: String, required: true },
+  id_number: { type: Number, required: true },
+  name: { type: String, required: true },
+  date_of_birth: { type: Date, required: true },
+  location: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
+users.pre("save", async function () {
+  this.email = this.email.toLowerCase();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+
+
 users.statics.authenticateBasic = async function (email, password) {
-	try {
-		const user = await this.findOne({ email });
-		if (!user) return ["The email doesn't exist", 404];
+  try {
+    const user = await this.findOne({ email });
+    if (!user) return ["The email doesn't exist", 404];
 
-		const valid = await bcrypt.compare(password, user.password);
-		if (valid) {
-			const payload = {
-				userId: user._id,
-				country: user.country,
-				role: user.role,
-			};
+    const valid = await bcrypt.compare(password, user.password);
+    if (valid) {
+      const payload = {
+        userId: user._id,
+        country: user.country,
+        role: user.role,
+      };
 
-			const options = {
-				expiresIn: '60m',
-			};
+      const options = {
+        expiresIn: "60m",
+      };
 
-			return [jwt.sign(payload, process.env.SECRET, options), 200];
-		}
-		return ['The password you’ve entered is incorrect', 403];
-	} catch (error) {
-		throw new Error(error.message);
-	}
+      return [jwt.sign(payload, process.env.SECRET, options), 200];
+    }
+    return ["The password you’ve entered is incorrect", 403];
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
-
 module.exports = mongoose.model("User", userSchema);
-
-
-
-
-
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const db = require('./db/db');
-
+const express = require("express");
+const cors = require("cors");
+// const db = require('./db/db');
+const bodyParser = require("body-parser");
+const { check, validationResult } = require("express-validator");
 
 const app = express();
-
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 //routers
 
 //built-in middlewares
@@ -14,14 +15,49 @@ app.use(express.json());
 app.use(cors());
 
 //app routers
-app.use('/users', usersRouter);
-app.use('/articles', articlesRouter);
-app.use(authRouter);
-app.use(commentsRouter);
-app.use(roleRouter);
+// app.use('/users', usersRouter);
+// app.use('/articles', articlesRouter);
+// app.use(authRouter);
+// app.use(commentsRouter);
+// app.use(roleRouter);
 
+// Set templating engine
+app.set("view engine", "ejs");
+
+// Navigation
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post(
+  "/register",
+  urlencodedParser,
+  [
+    check("id", "ID consist of 10 numbers")
+      .exists()
+      .isLength({ min: 10, max: 10 }),
+    check("email", "Email is not valid").exists().isEmail().normalizeEmail(),
+    check("password", "Please enter a strong Password")
+      .exists()
+      .isLength({ min: 8, max: 15 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+	if(!errors.isEmpty()){
+		// return res.status(422).jsonp(errors.array())
+		const alert = errors.array();
+		res.render('register', {alert})
+	}else{
+		user.save()
+	}
+  }
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-	console.log(`Server On ${PORT}`);
+  console.log(`Server On ${PORT}`);
 });
