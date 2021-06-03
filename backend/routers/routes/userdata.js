4fd534807b9e6055f6b76932c2017512c94506d3
Noof <noof.mohammed998@gmail.com>
2021-06-03 20:48:49 +0300

    getUserDataById_Number

--- backend/routers/routes/userdata.js
+++ backend/routers/routes/userdata.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const {createNewUserData} = require("../controllers/userdata")
+const {createNewUserData,getUserDataById_Number} = require("../controllers/userdata")
 
 const userDataRouter = express.Router();
 
 
 userDataRouter.post("/",createNewUserData)
+userDataRouter.get("/:id_Number",getUserDataById_Number)
 
 
 module.exports= userDataRouter;
