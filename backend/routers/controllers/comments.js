a822462a65eaab81ddea984adb530b9143dcd707
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-06-05 19:52:21 +0300

    update & delete comment (Asma & Noof )

--- backend/routers/controllers/comments.js
+++ backend/routers/controllers/comments.js
@@ -1,29 +1,67 @@
 const express = require("express");
-const Comment=require("../../db/models/comments.js")
-const article=require("../../db/models/articles");
+const Comment = require("../../db/models/comments.js");
+const article = require("../../db/models/articles");
 const articles = require("../../db/models/articles");
 
-
 //create new comment
-const create_new_comment= async(req, res) => {
-    let { comment, commenter } = req.body;
- let _id = req.params.id
-    const comment1 = new Comment({
-      comment,
-      commenter,
-    });
-    
-    await comment1
-    
-      .save()
-      .then(async(result) => {
-         await article.findOneAndUpdate({id :_id},{$push:{comments:result._id}},{new:true})
-        res.json(result);
-        status(201);
-      })
-      .catch((err) => {
-        res.send(err);
-      });
-  };
+const create_new_comment = async (req, res) => {
+  let { comment, commenter } = req.body;
+  let _id = req.params.id;
+  const comment1 = new Comment({
+    comment,
+    commenter,
+  });
 
-module.exports={create_new_comment}
+  await comment1
+
+    .save()
+    .then(async (result) => {
+      await article.findOneAndUpdate(
+        { id: _id },
+        { $push: { comments: result._id } },
+        { new: true }
+      );
+      res.json(result);
+      status(201);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
+
+//update comments
+const update_commentByID = (req, res) => {
+  let _id = req.params.id;
+
+  article.findOneAndUpdate(
+    { id: _id },
+    req.body,
+    { new: true }
+  ).then((result)=>{
+   res.json(result);
+    status(201); 
+  }).catch((err)=>{
+    res.send(err)
+  })
+};
+
+
+
+//delete comments
+
+const delete_commentById= (req,res)=>{
+  let _id=req.params.id
+article.findByIdAndRemove({id:_id})
+.then((result)=>{
+  res.json(result);
+   status(201); 
+ }).catch((err)=>{
+   res.send(err)
+ })
+}
+
+
+module.exports = {
+   create_new_comment,
+  update_commentByID ,
+  delete_commentById};
--- backend/routers/routes/comments.js
+++ backend/routers/routes/comments.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const {create_new_comment} =require("../controllers/comments")
+const {create_new_comment,update_commentByID,delete_commentById} =require("../controllers/comments")
 const commentRouter=express.Router()
 
-commentRouter.post("/",create_new_comment)
+
+ commentRouter.post("/",create_new_comment)
+ commentRouter.put("/:id",update_commentByID)
+ commentRouter.delete("/:id",delete_commentById)
+ 
 module.exports = commentRouter
